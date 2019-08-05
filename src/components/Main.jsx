e56da3bc1c13c0f1cdf9e70be154c6f717261bce
Angel Vega <angelvega85@gmail.com>
2019-08-05 16:27:53 -0700

    Added GIFs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import MainStyle from '../styled/MainStyle';
 import TwoSided from '../styled/TwoSided';
 import LoremIpsum from './LoremIpsum';
+import ConnectionPage from '../assets/connectionPage.gif';
+import TopicPage from '../assets/topicPage.gif';
+import BrokerView from '../assets/brokerView.gif';
 
 function Main() {
   return (
     <MainStyle>
       <TwoSided>
         <p>Connect to your cluster with any broker URI</p>
-        <img src="https://www.spearmintjs.com/static/appDemo-d08af9a9fbf6e5c58a520ce89a7d99ae.gif" alt=""/>
+        <img src={ConnectionPage} alt=""/>
       </TwoSided>
       <TwoSided>
-        <img src="https://www.spearmintjs.com/static/appDemo-d08af9a9fbf6e5c58a520ce89a7d99ae.gif" alt=""/>
+        <img src={TopicPage} alt=""/>
         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi</p>
+      </TwoSided>
+      <TwoSided>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi</p>
+        <img src={BrokerView} alt=""/>
       </TwoSided>
     </MainStyle>
   )
