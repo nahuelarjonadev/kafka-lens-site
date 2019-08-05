51c5f7c071024fa4997aade737e667213dca5289
gp5yk <25909829+gipsykeys@users.noreply.github.com>
2019-08-04 19:23:28 -0700

    create react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <Header />
+    <Main />
+    <Footer />
+  );
+}
+
+export default App;
