dda12aaed2ee795454da3f56c53a4f9682f2ee92
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-08-07 12:06:38 -0700

    pictures and names added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import BrianPicture from "../assets/pictures/brian.png";
+import SamHoPicture from "../assets/pictures/SamHo.png";
+import AngelPicture from "../assets/pictures/Angel.png";
+import NahuelPicture from "../assets/pictures/Nahuel.png";
 
 function Footer() {
-  return 'Footer'
+  return (
+    <div className="footer">
+      <h3>Meet the Team!</h3>
+      <div>
+        <img src={NahuelPicture} className="dev" />
+        <p>Nahuel Quimey Arjona Tennerini</p>
+      </div>
+      <div>
+        <img src={AngelPicture} className="dev" />
+        <p>Angel Kerridge</p>
+      </div>
+      <div>
+        <img src={BrianPicture} className="dev" />
+        <p>Brian Kwok</p>
+      </div>
+      <div>
+        <img src={SamHoPicture} className="dev" />
+        <p>Samuel Ho</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
