9cd432d2cbcb93a664736353c16fdfd7a0bbcfc9
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-08-07 21:42:58 -0700

    footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,21 +8,22 @@ function Footer() {
   return (
     <div className="footer">
       <h3>Meet the Team!</h3>
-      <div>
+      <div className="profilePic">
         <img src={NahuelPicture} className="dev" />
-        <p>Nahuel Quimey Arjona Tennerini</p>
+        <p>Nahuel Quimey Arjona Tennerini <br></br><a className="linkedin" href="https://www.linkedin.com/in/nahuel-quimey-arjona-tennerini/">@Nahuel</a></p>
       </div>
-      <div>
+      <div className="profilePic">
         <img src={AngelPicture} className="dev" />
-        <p>Angel Kerridge</p>
+        <p>Angel Evelyn Kerridge <br></br><a className="linkedin" href='https://www.linkedin.com/in/angelkerridge/'>@Angel</a></p>
       </div>
-      <div>
+      <div className="profilePic">
         <img src={BrianPicture} className="dev" />
-        <p>Brian Kwok</p>
+        <p>Brian Kwok<br></br><a className="linkedin" href="https://www.linkedin.com/in/briankwok15/">@Brian</a></p>
       </div>
-      <div>
+      <div className="profilePic">
         <img src={SamHoPicture} className="dev" />
-        <p>Samuel Ho</p>
+        <p>Samuel Ho<br></br><a className="linkedin" href="https://www.linkedin.com/in/samdho/">@Samuel</a></p>
+
       </div>
     </div>
   );
