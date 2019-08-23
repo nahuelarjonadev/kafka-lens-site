0ce49fac9489f343af7d2db5418139bb2415904b
Angel Vega <angelvega85@gmail.com>
2019-08-22 17:03:49 -0700

    Angel's descriptions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,19 +9,19 @@ function Footer() {
     <div className="footer">
       <h3>Meet the Team!</h3>
       <div className="profilePic">
-        <img src={NahuelPicture} className="dev" />
+        <img src={NahuelPicture} className="dev" alt="Nahuel Quimey Arjona Tennerini"/>
         <p>Nahuel Quimey Arjona Tennerini <br></br><a className="linkedin" href="https://www.linkedin.com/in/nahuel-quimey-arjona-tennerini/">@Nahuel</a></p>
       </div>
       <div className="profilePic">
-        <img src={AngelPicture} className="dev" />
+        <img src={AngelPicture} className="dev" alt="Angel Evelyn Kerridge"/>
         <p>Angel Evelyn Kerridge <br></br><a className="linkedin" href='https://www.linkedin.com/in/angelkerridge/'>@Angel</a></p>
       </div>
       <div className="profilePic">
-        <img src={BrianPicture} className="dev" />
+        <img src={BrianPicture} className="dev" alt="Brian Kwok"/>
         <p>Brian Kwok<br></br><a className="linkedin" href="https://www.linkedin.com/in/briankwok15/">@Brian</a></p>
       </div>
       <div className="profilePic">
-        <img src={SamHoPicture} className="dev" />
+        <img src={SamHoPicture} className="dev" alt="Samuel Ho"/>
         <p>Samuel Ho<br></br><a className="linkedin" href="https://www.linkedin.com/in/samdho/">@Samuel</a></p>
 
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainStyle from '../styled/MainStyle';
 import TwoSided from '../styled/TwoSided';
-import LoremIpsum from './LoremIpsum';
 import ConnectionPage from '../assets/connectionPage.gif';
 import TopicPage from '../assets/topicPage.gif';
 import BrokerView from '../assets/brokerView.gif';
@@ -10,16 +9,25 @@ function Main() {
   return (
     <MainStyle>
       <TwoSided>
-        <p>Connect to your cluster with any broker URI</p>
-        <img src={ConnectionPage} alt=""/>
+        <p>Connect to an existing Kafka cluster with any broker URI.</p>
+        <img src={ConnectionPage} alt="Kafka Lens connection page"/>
       </TwoSided>
       <TwoSided>
-        <img src={TopicPage} alt=""/>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi</p>
+        <img src={TopicPage} alt="Kafka Lens topic view"/>
+        <p>
+          Clicking on an individual topic expands the side menu to include that topic’s partitions. 
+          A secondary panel renders all the messages flowing through the topic in real time, with supplementary insight into the latest message. 
+          Selecting a specific partition further filters down the stream of messages. Additional partition metadata is displayed on a third panel.
+        </p>
       </TwoSided>
       <TwoSided>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi</p>
-        <img src={BrokerView} alt=""/>
+        <p>
+          Broker tiles provide the user with real time metrics such as which topics the broker is storing, partition leadership, 
+          message rates per second, and broker failure alerts.
+          <br/>
+          Clicking on a tile will open up a sidebar display of historical graph data of the selected broker message rates by topic.
+        </p>
+        <img src={BrokerView} alt="Kafka Lens broker view"/>
       </TwoSided>
     </MainStyle>
   )
